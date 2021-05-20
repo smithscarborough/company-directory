ce25e598c574cebcba0542994d88ebb701579b70
Smith Scarborough <smithscarborough@gmail.com>
2021-05-20 14:25:48 -0500

    refactor code w/ map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import Employee from './components/Employee';
 
 function App() {
+  const employeeInfo = [
+    {
+      firstName: 'John',
+      lastName: 'Smith',
+      phone: '555-555-5551',
+      ID: '1',
+    }, 
+    {
+      firstName: 'Bob',
+      lastName: 'Barker',
+      phone: '555-555-5552',
+      ID: '2',
+    },
+    {
+      firstName: 'Charlize',
+      lastName: 'Theron',
+      phone: '555-555-5553',
+      ID: '3',
+    },
+    {
+      firstName: 'Charles',
+      lastName: 'Barkley',
+      phone: '555-555-5554',
+      ID: '4',
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Company Directory</h1>
-        <Employee firstName="Smith" lastName="Scarborough" phone="555-555-5551" />
-        <Employee firstName="Bob" lastName="Barker" phone="555-555-5552" />
-        <Employee firstName="Charlize" lastName="Theron" phone="555-555-5554" />
-        <Employee firstName="Charles" lastName="Barkley" phone="555-555-5553" />
+        {employeeInfo.map((employee) => {
+            return <Employee key={employee.ID} {...employee} />;
+          })
+        }
       </header>
     </div>
   );
